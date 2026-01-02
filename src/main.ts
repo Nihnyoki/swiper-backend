5002776a9f7be9268f9d87d05d5979df38960a30
Sandile Joshua Innocent Mnqayi <sandile.mnqayi@Sandiles-MacBook-Air.local>
2026-01-02 10:55:55 +0200

    Added npx to build command

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  //await app.listen(3000);
 }
 bootstrap();
