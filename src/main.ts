d72b0c0f8478747e89014ebbe0cbb2740635a17a
Sandile Mnqayi <wifizi.gokhinki@gmail.com>
2026-01-03 08:35:21 +0200

    origin: and Access-Control-Allow-Origin

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('My API')
-    .setDescription('API docs')
+    .setTitle('Swiper Backend')
+    .setDescription('Swiper API docs')
     .setVersion('1.0')
     .build();
 
@@ -15,14 +15,9 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors({ 
-   origin: [
-      'https://swiper-cards.up.railway.app/',
-      'https://swiper-cards.up.railway.app',
-      'swiper-cards.up.railway.app'
-    ],
+   origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin'],
-    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
   });
 
   const port = process.env.PORT || 10000;
